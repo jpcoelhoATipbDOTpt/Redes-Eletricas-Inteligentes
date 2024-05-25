998e36fc94003887709fbcf7ca214f3eed350526
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2024-05-25 20:54:37 +0100

    Add files via upload

---
+++ ESP32/Testa_ESP32.ino
@@ -0,0 +1,24 @@
+// REDES ELÉTRICAS INTELIGENTES
+//
+// Código para testar a cadeia de compilação e gravação do programa no ESP32
+// Placa: "ESP32 Dev Module"
+// 
+// Adaptado de "Exemplos/Blink"
+//
+// Depois de compilado e carregado para a plataforma de desenvolvimento ESP32, 
+// o LED existente na placa deve piscar com uma frequência de 0.5Hz.
+//
+// 2023/2024
+
+#define LED_BUILTIN 2
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
