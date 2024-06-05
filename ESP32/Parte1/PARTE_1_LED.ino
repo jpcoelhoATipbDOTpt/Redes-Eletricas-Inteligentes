ad58b4886a4fe4eb08bb0f1ed59ea316546918de
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2024-06-05 11:28:03 +0100

    Update PARTE_1_LED.ino

--- ESP32/Parte1/PARTE_1_LED.ino
+++ ESP32/Parte1/PARTE_1_LED.ino
@@ -33,8 +33,6 @@ PubSubClient client(espClient);                   // Cria objeto MQTT
 void setup() {
   Serial.begin(115200);                           // Define interface de comunicação série
   pinMode(LED_BUILTIN, OUTPUT);                   // Define pino LED_BUILTIN como saída
-  id_do_cliente += String(WiFi.macAddress());     // MAC tem 6 bytes + 5 separadores
-  Serial.printf("Olá. Sou o nó %s\n", id_do_cliente.c_str());
   //-----------------------------------------------------------------
   // Estabelece ligação WiFi
   //-----------------------------------------------------------------
@@ -45,6 +43,11 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("concluído.");
+  //--------------------------------------------------------------------
+  // ID do nó
+  //-------------------------------------------------------------------- 
+  id_do_cliente += String(WiFi.macAddress());     // MAC tem 6 bytes + 5 separadores
+  Serial.printf("Olá. Sou o nó %s\n", id_do_cliente.c_str());  
   //--------------------------------------------------------------------
   // Define broker MQTT, porto de escuta e rotina de callback
   //--------------------------------------------------------------------
