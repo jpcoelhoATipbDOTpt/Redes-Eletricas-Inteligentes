25d0c861119d18373a3343d4bdc3daf66321a2cc
Joao Paulo Coelho <60382778+jpcoelhoATipbDOTpt@users.noreply.github.com>
2024-06-06 12:24:08 +0100

    Update Testa_ESP32.ino

--- ESP32/Testa_ESP32.ino
+++ ESP32/Testa_ESP32.ino
@@ -11,14 +11,18 @@
 // 2023/2024
 
 #define LED_BUILTIN 2
+unsigned int cnt = 0;
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
+  Serial.println(cnt);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
+  cnt++;
 }
